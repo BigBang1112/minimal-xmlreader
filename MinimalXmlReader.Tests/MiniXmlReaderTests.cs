30f5104c231fabe5daa3469c434ae740784f7577
Petr Pivoňka <petrpiv1@gmail.com>
2023-07-27 00:16:24 +0200

    Přidejte soubory projektu.

---
+++ MinimalXmlReader.Tests/MiniXmlReaderTests.cs
@@ -0,0 +1,47 @@
+﻿namespace MinimalXmlReader.Tests;
+
+public class MiniXmlReaderTests
+{
+    [Fact]
+    public void Test()
+    {
+        var xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<methodCall>
+<methodName>TrackMania.PlayerChat</methodName>
+<params>
+<param><value><i4>0</i4></value></param>
+<param><value><string>bigbang1112_dev</string></value></param>
+<param><value><string>TrackMania.PlayerFinish 237 bigbang1112 0</string></value></param>
+<param><value><boolean>0</boolean></value></param>
+</params>
+</methodCall>";
+
+        var reader = new MiniXmlReader(xml);
+
+        reader.SkipProcessingInstruction();
+        reader.SkipStartElement("methodCall");
+        reader.SkipStartElement("methodName");
+        var methodName = reader.ReadContent();
+        reader.SkipEndElement();
+        reader.SkipStartElement("params");
+
+        while (reader.SkipStartElement("param"))
+        {
+            reader.SkipStartElement("value");
+
+            switch (reader.ReadStartElement())
+            {
+                case "i4":
+                    var i4 = reader.ReadContent();
+                    break;
+                default:
+                    var bla = reader.ReadContent();
+                    break;
+            }
+
+            reader.SkipEndElement();
+            reader.SkipEndElement();
+            reader.SkipEndElement();
+        }
+    }
+}
---
+++ MinimalXmlReader/MiniXmlReader.cs
@@ -0,0 +1,163 @@
+﻿using System.Diagnostics;
+
+namespace MinimalXmlReader;
+
+public sealed class MiniXmlReader
+{
+    private readonly string xml;
+    private int position;
+
+    public MiniXmlReader(string xml)
+    {
+        this.xml = xml;
+    }
+
+    public void SkipProcessingInstruction()
+    {
+        SkipSpaces();
+
+        _ = SkipChar('<');
+        _ = SkipChar('?');
+        SkipUntilChar(' ');
+
+        do
+        {
+            while (xml[position] != '?') position++;
+            position++;
+        }
+        while (!SkipChar('>'));
+    }
+
+    private bool SkipChar(char c)
+    {
+        var skipped = true;
+
+        Debug.WriteLine($"SkipChar({c}) actual: {xml[position]}");
+
+        if (xml[position] != c) skipped = false;
+        position++;
+        return skipped;
+    }
+
+    private void SkipUntilChar(char c)
+    {
+        do
+        {
+            var ch = xml[position];
+            if (ch == c) return;
+            if (ch == '\r' || ch == '\n') throw new Exception();
+            position++;
+        }
+        while (true);
+    }
+
+    private bool SkipSpaces()
+    {
+        var skipped = false;
+
+        do
+        {
+            var c = xml[position];
+            if (c != ' ' && c != '\t' && c != '\r' && c != '\n') return skipped;
+            position++;
+            skipped = true;
+        }
+        while (true);
+    }
+
+    public bool SkipStartElement(string name)
+    {
+        SkipSpaces();
+
+        var safePosition = position;
+
+        _ = SkipChar('<');
+
+        for (var i = 0; i < name.Length; i++)
+        {
+            var ch = xml[position];
+
+            if (ch != name[i])
+            {
+                position = safePosition;
+                return false;
+            }
+
+            position++;
+        }
+
+        SkipSpaces();
+
+        if (SkipChar('>'))
+        {
+            return true;
+        }
+
+        throw new Exception();
+
+        // No attributes expected yet
+    }
+
+    public string ReadStartElement()
+    {
+        SkipSpaces();
+
+        _ = SkipChar('<');
+
+        var start = position;
+
+        while (xml[position] != ' ' && xml[position] != '>') position++;
+
+        var name = xml[start..position];
+
+        SkipSpaces();
+
+        position++;
+
+        return name;
+    }
+
+    public void SkipEndElement(string name)
+    {
+        SkipSpaces();
+
+        _ = SkipChar('<');
+        _ = SkipChar('/');
+
+        for (var i = 0; i < name.Length; i++)
+        {
+            var ch = xml[position];
+            if (ch != name[i]) throw new Exception();
+            position++;
+        }
+
+        SkipSpaces();
+
+        if (!SkipChar('>'))
+        {
+            throw new Exception();
+        }
+    }
+
+    public void SkipEndElement()
+    {
+        SkipSpaces();
+
+        _ = SkipChar('<');
+        _ = SkipChar('/');
+        SkipUntilChar('>');
+
+        position++;
+    }
+
+    public string ReadContent()
+    {
+        SkipSpaces();
+
+        var start = position;
+
+        while (xml[position] != '<') position++;
+
+        return xml[start..position];
+    }
+}
